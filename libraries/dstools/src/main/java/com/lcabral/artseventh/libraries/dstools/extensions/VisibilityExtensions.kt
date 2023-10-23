1db5efdbcfdb0c7c97fd6daf3a81ef1bf58767d0
LeeLoo <63366380+LucianaCabral@users.noreply.github.com>
2023-10-23 08:48:22 -0300

    Added:[ASA-5] Criar Ds (#7)
    * Added:[ASA-5] Create design lib
    
    * Added:[ASA-5] Add drawable design lib

---
+++ libraries/dstools/src/main/java/com/lcabral/artseventh/libraries/dstools/extensions/VisibilityExtensions.kt
@@ -0,0 +1,5 @@
+package com.lcabral.artseventh.libraries.dstools.extensions
+
+public fun android.view.View.gone(): kotlin.Unit{}
+public fun android.view.View.invisible(): kotlin.Unit{}
+public fun android.view.View.visible(): kotlin.Unit{}
