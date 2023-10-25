e378661842eb9810cb84f4bb1ae6d05d1b80fb4f
LucianaCabral <eng.luciana.cabral@gmail.com>
2023-10-25 08:17:41 -0300

    Added:[ASA-7-1] Create and implement local module

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -3,5 +3,8 @@ object Modules {
     const val archLibs = ":libraries:arch"
 
     // core
+    const val domain = ":core:common:domain"
+    const val local = ":core:data:local"
+    const val navigation = ":core:common:navigation"
     const val remote = ":core:data:remote"
 }
