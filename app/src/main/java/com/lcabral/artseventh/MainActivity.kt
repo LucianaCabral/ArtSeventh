79c352f8e21f4eed2e7c0db9ec56e508997531f8
LucianaCabral <eng.luciana.cabral@gmail.com>
2023-10-25 23:02:13 -0300

    Added:[ASA-9-2] Add dashboard module

--- app/src/main/java/com/lcabral/artseventh/MainActivity.kt
+++ app/src/main/java/com/lcabral/artseventh/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.lcabral.artseventh
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import com.lcabral.artseventh.core.common.navigation.DashboardNavigation
+import com.lcabral.artseventh.databinding.ActivityMainBinding
+import org.koin.android.ext.android.inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var binding: ActivityMainBinding
+    private val dashboardNavigation: DashboardNavigation by inject()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        window.statusBarColor = Color.BLACK
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                replace(R.id.container_main, dashboardNavigation.create())
+            }
+        }
     }
 }
