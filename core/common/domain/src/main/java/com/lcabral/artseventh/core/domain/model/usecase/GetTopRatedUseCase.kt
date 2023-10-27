6315ec97ca04ac3d0ff1e6e46b7277d09d8885f9
LucianaCabral <eng.luciana.cabral@gmail.com>
2023-10-27 09:55:06 -0300

    Added:[ASA-15-2] Indenta usecase upcoming in domain

--- core/common/domain/src/main/java/com/lcabral/artseventh/core/domain/model/usecase/GetTopRatedUseCase.kt
+++ core/common/domain/src/main/java/com/lcabral/artseventh/core/domain/model/usecase/GetTopRatedUseCase.kt
@@ -1,5 +1,9 @@
 package com.lcabral.artseventh.core.domain.model.usecase
 
+import com.lcabral.artseventh.core.domain.model.Movie
+import com.lcabral.artseventh.core.domain.model.repository.MovieRepository
+import kotlinx.coroutines.flow.Flow
+
 internal class GetTopRatedUseCase(private val repository: MovieRepository) {
     operator fun invoke(): Flow<List<Movie>> = repository.getTopRated()
 }
