36c171266f3180d9a4b04c5a3bcd317321cce947
LucianaCabral <eng.luciana.cabral@gmail.com>
2023-10-30 07:30:10 -0300

    Fixed:[ASA-18-1] Refactor method name

---
+++ core/common/domain/src/main/java/com/lcabral/artseventh/core/domain/model/usecase/SetFavoriteUseCase.kt
@@ -0,0 +1,12 @@
+package com.lcabral.artseventh.core.domain.model.usecase
+
+import com.lcabral.artseventh.core.domain.model.Movie
+import com.lcabral.artseventh.core.domain.model.repository.MovieRepository
+
+class SetFavoriteUseCase(
+    private val repository: MovieRepository,
+) {
+    suspend operator fun invoke(movie:Movie): Long {
+        return repository.insertMovie(movie)
+    }
+}
