f790fd01600896e53561549ff43d584b87774cba
LucianaCabral <eng.luciana.cabral@gmail.com>
2023-11-18 05:28:22 -0300

    Fixed:[ASA-ANY] Fixed layout

--- features/search/src/main/java/com/lcabral/artseventh/features/search/navigation/SearchNavigationImpl.kt
+++ features/search/src/main/java/com/lcabral/artseventh/features/search/navigation/SearchNavigationImpl.kt
@@ -1,9 +1,9 @@
 package com.lcabral.artseventh.features.search.navigation
 
 import android.content.Context
+import com.lcabral.arch.lib.extensions.createIntent
 import com.lcabral.artseventh.core.common.navigation.SearchNavigation
 import com.lcabral.artseventh.features.search.SearchActivity
-import com.lcabral.artseventh.libraries.arch.extensions.createIntent
 
 internal class SearchNavigationImpl : SearchNavigation {
     override fun create(context: Context) {
